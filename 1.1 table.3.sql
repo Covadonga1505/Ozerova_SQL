bdccc89eab9aaa5605ee4a5ceac564ee9aee32bd
Olga Ustiantseva <89701996+Covadonga1505@users.noreply.github.com>
2023-02-02 10:06:03 +0300

    Update 1.1 table.3.sql

--- 1.1 table.3.sql
+++ 1.1 table.3.sql
@@ -1,4 +1,6 @@
 INSERT INTO book (title, author, price, amount) VALUES('Белая гвардия', 'Булгаков М.А.','540.50', '5');
 INSERT INTO book (title, author, price, amount) VALUES('Идиот', 'Достоевский Ф.М.','460.00', '10');
 INSERT INTO book (title, author, price, amount) VALUES('Братья Карамазовы', 'Достоевский Ф.М.','799.01', '2');
+INSERT INTO book (title, author, price, amount) VALUES('Капитанская дочка', 'Пушкин А.С.','Null', 'Null');
+INSERT INTO book (title, author, price, amount) VALUES('Стихотворения и поэмы', 'Есенин С.А.','Null', 'Null');
 SELECT * FROM book;
